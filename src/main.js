87d16b5c2e5ff13e89d6f3b693d5a43ce7f0e4a7
Vadagon <verbalike@gmail.com>
2019-03-29 01:52:17 +0200

    1/3 of 1st part of layout

--- src/main.js
+++ src/main.js
@@ -2,18 +2,24 @@ import Vue from 'vue'
 import App from './App.vue'
 import Vuetify from 'vuetify'
 import router from './router'
+import Vuex from 'vuex'
 
 import 'vuetify/dist/vuetify.min.css'
 import './styles/main.scss'
 
 Vue.use(Vuetify)
+Vue.use(Vuex)
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  data: {
+const store = new Vuex.Store({
+  state: {
+    count: 2
+  }
+})
 
-  },
+window.app = new Vue({
+  router,
+  store,
   render: h => h(App)
 }).$mount('#app')
